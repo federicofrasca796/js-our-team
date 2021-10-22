66f510247bf27cda970fa34be4395cf668e55256
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-22 15:06:57 +0200

    add html struct and js instructions

---
+++ main.js
@@ -0,0 +1,6 @@
+/* Descizione
+Il nostro compito é quello di rappresentare, utilizzando la struttura dati piú appropriata, una lista di membri di un team.
+Ciascun membro del team avrá come caratteristica name, role, image
+
+Todo:
+- Stampare in console tutti gli elementi del team e le loro proprieta. */
